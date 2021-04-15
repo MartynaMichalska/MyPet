e868d79fa20f802fe368066d64be7c2b4c4575bb
Weronika <laskowska29@gmail.com>
2021-04-15 15:24:33 +0200

    choose_gender Done

---
+++ app/src/main/java/com/example/petcare/petInfo01.java
@@ -0,0 +1,14 @@
+package com.example.petcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class petInfo01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_info01);
+    }
+}

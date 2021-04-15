190aa5edbcb684eb5fe0eb4122c6ea5305b340ab
Weronika <laskowska29@gmail.com>
2021-04-15 14:55:20 +0200

    activity_add Done Choose_Race Done addings

--- app/src/main/java/com/example/petcare/Choose_Race.java
+++ app/src/main/java/com/example/petcare/Choose_Race.java
@@ -2,13 +2,38 @@ package com.example.petcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Choose_Race extends AppCompatActivity {
+
+    private ImageView dog;
+    private ImageView cat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose__race);
+        setContentView(R.layout.activity_choose_race);
+        dog= (ImageView) findViewById(R.id.doggie);
+        cat= (ImageView) findViewById(R.id.cat);
+        dog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+        cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+    public void openActivity ()
+    {
+        Intent intent = new Intent (this, pickGender.class);
+        startActivity(intent);
     }
 }

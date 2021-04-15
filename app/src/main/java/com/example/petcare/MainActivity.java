fe096e67fdd4eaafa2b0f3ae0519b9f5c52fe3f7
Martyna <m.michalska4126@gmail.com>
2021-04-15 16:15:54 +0200

    logging in works

--- app/src/main/java/com/example/petcare/MainActivity.java
+++ app/src/main/java/com/example/petcare/MainActivity.java
@@ -17,10 +17,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.auth.User;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button logi;
     private Button signu;
     private FirebaseAuth mAuth;
 
@@ -29,39 +28,71 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        EditText email = findViewById(R.id.login_email);
-        EditText password = findViewById(R.id.login_haslo);
+        EditText email = findViewById(R.id.registration_email_input);
+        EditText password = findViewById(R.id.registration_password_input);
+        EditText L_password = findViewById(R.id.login_password);
+        EditText L_email = findViewById(R.id.login_email);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.log_in);
+        //setContentView(R.layout.activity_main);
+        logi = (Button) findViewById(R.id.log_in);
         signu = (Button) findViewById(R.id.sign_up);
-        //mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString());
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-
-            public void onClick(View v) {
-                openActivity2();
-            }
-        });
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        EditText email_input = findViewById(R.id.login_email);
+        EditText password_input = findViewById(R.id.login_password);
+        Button login_button = findViewById(R.id.log_in);
+        Button signup_button = findViewById(R.id.sign_up);
 
 
-        signu.setOnClickListener(new View.OnClickListener() {
+        signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity3();
             }
         });
 
+        login_button.setOnClickListener((v) -> loginUser(email_input.getText().toString(), password_input.getText().toString()));
     }
 
-    public void openActivity2 ()
+    private void loginUser(String L_email, String L_password)
     {
+        if (L_email.isEmpty() || L_password.isEmpty() )
+        {
+            Toast.makeText(this, "No blank space allowed!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (L_password.length()<7 )
+        {
+            Toast.makeText(this, "Password must be at least 7 characters long ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if ( !L_email.contains("@"))
+        {
+            Toast.makeText(this, "Wrong email format", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(L_email, L_password)
+                .addOnCompleteListener(this, this::handleLogin);
 
-        Intent intent = new Intent (this, Your_account2.class);
-        startActivity(intent);
     }
+    private void handleLogin(Task<AuthResult> authResultTask) {
+        if (authResultTask.isSuccessful()) {
+            Intent intent = new Intent (this, Your_account2.class);
+            startActivity(intent);
+
+        }
+        else {
+            Toast.makeText(this, "Error:"+authResultTask.getException(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+
+
+
+
+
 
     public void openActivity3 ()
     {

3920d1677fed47283eb3e90aa741363084c0cfc2
Weronika <laskowska29@gmail.com>
2021-05-28 17:59:35 +0200

    new article added, some buttons added, pictures yet to be added to git ( don't show up on other devices )

--- app/src/main/java/com/example/petcare/faqCatFeeding.java
+++ app/src/main/java/com/example/petcare/faqCatFeeding.java
@@ -4,19 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class faqCatFeeding extends AppCompatActivity {
 private ImageView dry;
 private ImageView train;
+private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq_cat_feeding);
         dry=(ImageView) findViewById(R.id.dryFood);
         train=(ImageView) findViewById(R.id.transitionCat);
+        bt=(Button) findViewById(R.id.backToMainFaqqq);
         dry.setOnClickListener(v -> openActivityDry());
         train.setOnClickListener(v -> openActivityTrans());
+        bt.setOnClickListener(v -> openActivityBack());
+    }
+
+    private void openActivityBack() {
+        Intent intent = new Intent(this, faqCatsMain.class);
+        startActivity(intent);
     }
 
     private void openActivityTrans() {

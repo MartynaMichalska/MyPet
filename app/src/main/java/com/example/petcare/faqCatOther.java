75fcbd7e13611d9933ec1c2d6ca1e955bb04a8a0
Weronika <laskowska29@gmail.com>
2021-06-05 11:57:03 +0200

    last article

--- app/src/main/java/com/example/petcare/faqCatOther.java
+++ app/src/main/java/com/example/petcare/faqCatOther.java
@@ -3,12 +3,22 @@ package com.example.petcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class faqCatOther extends AppCompatActivity {
+    private Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq_cat_other);
+        bt=(Button) findViewById(R.id.backToMainFaq6);
+       bt.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               onBackPressed();
+           }
+       });
     }
 }

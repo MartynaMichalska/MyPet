c9302b6bf482b0c04c6f859daf1f1cd3517845d4
Martyna <m.michalska4126@gmail.com>
2021-06-07 21:18:03 +0200

    faq completed, layout touchups done

--- app/src/main/java/com/example/petcare/faqDogFeeding.java
+++ app/src/main/java/com/example/petcare/faqDogFeeding.java
@@ -1,6 +1,25 @@
 package com.example.petcare;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class faqDogFeeding extends AppCompatActivity {
+    private Button bt;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faq_dog_food);
+        bt = findViewById(R.id.backDogsFeeding);
+        bt.setOnClickListener(v -> backToMain());
+    }
+
+    private void backToMain() {
+        Intent intent = new Intent(this, faqDogsMain.class);
+        startActivity(intent);
+    }
 }
